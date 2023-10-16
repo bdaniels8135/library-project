4680d59e5f71d42f708bc90d2bee459ab485ef32
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-10-16 08:54:31 -0400

    Refactor library into object and add delete book

--- script.js
+++ script.js
@@ -2,22 +2,41 @@ function Book(title, author, haveRead) {
     this.title = title;
     this.author = author;
     this.haveRead = haveRead;
+    this.libraryIndex = null;
 };
 
-const myLibrary = [];
+const myLibrary = {
+    books: [],
+    _nextBookID: 0,
+
+    addBook(newBook) {
+        newBook.libraryIndex = myLibrary._nextBookID;
+        myLibrary._nextBookID += 1;
+        myLibrary.books.push(newBook);
+        refreshLibraryCards();
+    },
+
+    removeBook(bookLibraryIndex) {
+        myLibrary.books = myLibrary.books.filter((book) => {
+            return book.libraryIndex != bookLibraryIndex
+        })
+        refreshLibraryCards();
+    },
+}
+
 const main = document.querySelector('main');
 const newBookButton = document.querySelector('header img');
 
-function createNewCard(newBook) {
+function createNewCard(libraryBook) {
     const cardElem = document.createElement('div');
     cardElem.classList.add('card');
-    cardElem.setAttribute('id', `card-${myLibrary.length}`)
+    cardElem.setAttribute('id', `card-${libraryBook.libraryIndex}`)
     const titleElem = document.createElement('p');
     titleElem.classList.add('title');
-    titleElem.innerText = `${newBook.title}`;
+    titleElem.innerText = `${libraryBook.title}`;
     const authorElem = document.createElement('p');
     authorElem.classList.add('author');
-    authorElem.innerText = `${newBook.author}`;
+    authorElem.innerText = `${libraryBook.author}`;
     const checkWrapperElem = document.createElement('div');
     checkWrapperElem.classList.add('checkbox-wrapper');
     const checkLabelElem = document.createElement('p');
@@ -25,11 +44,11 @@ function createNewCard(newBook) {
     const inputElem = document.createElement('input');
     inputElem.setAttribute('type', 'checkbox');
     inputElem.setAttribute('name', 'read-book-check');
-    inputElem.setAttribute('id', `read-book-check-${myLibrary.length}`);
-    if (newBook.haveRead) {inputElem.setAttribute('checked', '')};
+    inputElem.setAttribute('id', `read-book-check-${libraryBook.libraryIndex}`);
+    if (libraryBook.haveRead) {inputElem.setAttribute('checked', '')};
     const btnElem = document.createElement('button');
     btnElem.setAttribute('type', 'button');
-    btnElem.setAttribute('id', `trash-button-${myLibrary.length}`)
+    btnElem.setAttribute('id', `trash-button-${libraryBook.libraryIndex}`)
     const imgElem = document.createElement('img');
     imgElem.setAttribute('src', './img/trash-can-outline.svg');
     imgElem.setAttribute('alt', 'Trash Icon');
@@ -45,23 +64,22 @@ function createNewCard(newBook) {
     return cardElem;
 }
 
-function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
-}
-
 function refreshLibraryCards() {
-    for (let book of myLibrary) {
-        main.appendChild(createNewCard(book));
+    main.innerHTML = '';
+    for (let book of myLibrary.books) {
+        const newCard = createNewCard(book);
+        const trashBtn = newCard.querySelector(`#trash-button-${book.libraryIndex}`);
+        trashBtn.addEventListener('click', (event) => {
+            const bookLibraryIndex = Number(event.target.parentElement.id.slice(-1));
+            myLibrary.removeBook(bookLibraryIndex);
+        });
+        main.appendChild(newCard);
     }
 }
 
-
-
 // Add some books by default
-
-addBookToLibrary(new Book('Dune', 'Frank Herbert', true));
-addBookToLibrary(new Book('Grapes of Wrath', 'John Steinbeck', false));
-addBookToLibrary(new Book('Where the Sidewalk Ends', 'Shel Silverstein', false));
-addBookToLibrary(new Book('Orlando', 'Virginia Woolf', false));
-addBookToLibrary(new Book('Throne of Glass', 'Sarah J. Maas', true));
-refreshLibraryCards();
+myLibrary.addBook(new Book('Dune', 'Frank Herbert', true));
+myLibrary.addBook(new Book('Grapes of Wrath', 'John Steinbeck', false));
+myLibrary.addBook(new Book('Where the Sidewalk Ends', 'Shel Silverstein', false));
+myLibrary.addBook(new Book('Orlando', 'Virginia Woolf', false));
+myLibrary.addBook(new Book('Throne of Glass', 'Sarah J. Maas', true));
