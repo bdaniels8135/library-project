189185cbf1d4970968f7d8f98fc6cd43adbf172a
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-10-16 07:33:50 -0400

    Create refresh library function

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ function Book(title, author, haveRead) {
     this.haveRead = haveRead;
 };
 
-const MY_LIBRARY = [];
-
-const MAIN = document.querySelector('main');
-
-function addBookToLibrary(newBook) {
-    MY_LIBRARY.push(newBook);
-    MAIN.appendChild(createNewCard(newBook));
-}
+const myLibrary = [];
+const main = document.querySelector('main');
+const newBookButton = document.querySelector('header img');
 
 function createNewCard(newBook) {
     const cardElem = document.createElement('div');
     cardElem.classList.add('card');
+    cardElem.setAttribute('id', `card-${myLibrary.length}`)
     const titleElem = document.createElement('p');
     titleElem.classList.add('title');
     titleElem.innerText = `${newBook.title}`;
@@ -28,11 +24,12 @@ function createNewCard(newBook) {
     checkLabelElem.innerText = 'Already Read:';
     const inputElem = document.createElement('input');
     inputElem.setAttribute('type', 'checkbox');
-    inputElem.setAttribute('name', 'read-book');
-    inputElem.setAttribute('id', `read-book-${MY_LIBRARY.length}`);
+    inputElem.setAttribute('name', 'read-book-check');
+    inputElem.setAttribute('id', `read-book-check-${myLibrary.length}`);
     if (newBook.haveRead) {inputElem.setAttribute('checked', '')};
     const btnElem = document.createElement('button');
     btnElem.setAttribute('type', 'button');
+    btnElem.setAttribute('id', `trash-button-${myLibrary.length}`)
     const imgElem = document.createElement('img');
     imgElem.setAttribute('src', './img/trash-can-outline.svg');
     imgElem.setAttribute('alt', 'Trash Icon');
@@ -48,12 +45,23 @@ function createNewCard(newBook) {
     return cardElem;
 }
 
-const newBookButton = document.querySelector('header img');
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
+
+function refreshLibraryCards() {
+    for (let book of myLibrary) {
+        main.appendChild(createNewCard(book));
+    }
+}
+
+
 
 // Add some books by default
 
-addBookToLibrary(new Book('Dune', 'Frank Herbert', true))
-addBookToLibrary(new Book('Grapes of Wrath', 'John Steinbeck', false))
-addBookToLibrary(new Book('Where the Sidewalk Ends', 'Shel Silverstein', false))
-addBookToLibrary(new Book('Orlando', 'Virginia Woolf', false))
-addBookToLibrary(new Book('Throne of Glass', 'Sarah J. Maas', true))
+addBookToLibrary(new Book('Dune', 'Frank Herbert', true));
+addBookToLibrary(new Book('Grapes of Wrath', 'John Steinbeck', false));
+addBookToLibrary(new Book('Where the Sidewalk Ends', 'Shel Silverstein', false));
+addBookToLibrary(new Book('Orlando', 'Virginia Woolf', false));
+addBookToLibrary(new Book('Throne of Glass', 'Sarah J. Maas', true));
+refreshLibraryCards();
