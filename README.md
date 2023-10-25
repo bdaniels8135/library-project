269c8d3eecfee32fd35e092b8b7158d5aee0c356
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-10-25 10:05:22 -0400

    Refactor code to use classes and closures

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This is an assignment from the [Odin Project](https://www.theodinproject.com/).
 
 [Here](https://www.theodinproject.com/lessons/node-path-javascript-library) is the project brief.
 
-[Here](https://bdaniels8135.github.io/library-project/) is a live preview of the page.
+[Here](https://bdaniels8135.github.io/library-project/) is a live preview of the my submission.
+
+The original assignment involved basic object literals. Later in the course we were encouraged to revisit our code to implement classes, closures, etc. as appropriate.
--- script.js
+++ script.js
@@ -1,77 +1,23 @@
-function Book(title, author, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.haveRead = haveRead;
-    this._id = null;
+class Book {
+    constructor(title, author, haveRead) {
+        this.title = title;
+        this.author = author;
+        this.haveRead = haveRead;
+        this.id;
+    };
 };
 
-const ui = {
-    _main: document.querySelector('main'),
-    _newBookButton: document.querySelector('header img'),
-    _newBookModal: document.querySelector('dialog'),
-    _newBookTitle: document.getElementById('new-book-title'),
-    _newBookAuthor: document.getElementById('new-book-author'),
-    _newBookRead: document.getElementById('new-book-read'),
-    _cancelBtn: document.getElementById('cancel-btn'),
-    _submitBtn: document.getElementById('submit-btn'),
-
-    initialize() {
-        ui._newBookButton.addEventListener('click', () => {
-            ui._newBookModal.showModal();
-        })
-
-        ui._cancelBtn.addEventListener('click', () => {
-            ui._newBookModal.close();
-            ui._newBookTitle.value = '';
-            ui._newBookAuthor.value = '';
-            ui._newBookRead.checked = false;
-        })
-
-        ui._submitBtn.addEventListener('click', (event) => {
-            event.preventDefault();
-            ui._newBookModal.close();
-            const newTitle = ui._newBookTitle.value;
-            const newAuthor = ui._newBookAuthor.value;
-            const newRead = ui._newBookRead.checked;
-            myLibrary.addBook(new Book(newTitle, newAuthor, newRead));
-            ui._newBookTitle.value = '';
-            ui._newBookAuthor.value = '';
-            ui._newBookRead.checked = false;
-        })
-    },
-
-    refreshLibraryCards() {
-        ui._main.innerHTML = '';
-        for (let book of myLibrary.books) {
-            const newCard = ui.createNewCard(book);
-            ui.addDeleteEventListener(newCard, book);
-            ui.addReadCheckEventListener(newCard, book);
-            ui._main.appendChild(newCard);
-        }
-    },
-
-    addDeleteEventListener(card, book) {
-        card.querySelector('button').addEventListener('click', () => {
-                myLibrary.removeBook(book);
-            });
-    },
-
-    addReadCheckEventListener(card, book) {
-        card.querySelector('input').addEventListener('change', () => {
-                myLibrary.toggleHaveRead(book);
-        })
-    },
-
-    createNewCard(libraryBook) {
-        const cardElem = document.createElement('div');
-        cardElem.classList.add('card');
-        cardElem.setAttribute('id', `card-${libraryBook._id}`)
+class Card {
+    constructor(book) {
+        this.html = document.createElement('div');
+        this.html.classList.add('card');
+        this.html.setAttribute('id', `card-${book._id}`);
         const titleElem = document.createElement('p');
         titleElem.classList.add('title');
-        titleElem.innerText = `${libraryBook.title}`;
+        titleElem.innerText = `${book.title}`;
         const authorElem = document.createElement('p');
         authorElem.classList.add('author');
-        authorElem.innerText = `${libraryBook.author}`;
+        authorElem.innerText = `${book.author}`;
         const checkWrapperElem = document.createElement('div');
         checkWrapperElem.classList.add('checkbox-wrapper');
         const checkLabelElem = document.createElement('p');
@@ -79,54 +25,112 @@ const ui = {
         const inputElem = document.createElement('input');
         inputElem.setAttribute('type', 'checkbox');
         inputElem.setAttribute('name', 'read-book-check');
-        inputElem.setAttribute('id', `read-book-check-${libraryBook._id}`);
-        if (libraryBook.haveRead) {inputElem.setAttribute('checked', '')};
+        inputElem.setAttribute('id', `read-book-check-${book._id}`);
+        if (book.haveRead) inputElem.setAttribute('checked', '');
         const btnElem = document.createElement('button');
         btnElem.setAttribute('type', 'button');
-        btnElem.setAttribute('id', `trash-button-${libraryBook._id}`)
+        btnElem.setAttribute('id', `trash-button-${book._id}`)
         const imgElem = document.createElement('img');
         imgElem.setAttribute('src', './img/trash-can-outline.svg');
         imgElem.setAttribute('alt', 'Trash Icon');
     
-        cardElem.appendChild(titleElem);
-        cardElem.appendChild(authorElem);
-        cardElem.appendChild(checkWrapperElem);
-        cardElem.appendChild(btnElem);
+        this.html.appendChild(titleElem);
+        this.html.appendChild(authorElem);
+        this.html.appendChild(checkWrapperElem);
+        this.html.appendChild(btnElem);
         checkWrapperElem.appendChild(checkLabelElem);
         checkWrapperElem.appendChild(inputElem);
         btnElem.appendChild(imgElem);
-    
-        return cardElem;
-    }
-}
+    };
+};
 
-const myLibrary = {
-    books: [],
-    _nextBookId: 0,
+const displayController = (function() {
+    const _UI = {
+        MAIN: document.querySelector('main'),
+        NEW_BOOK_BUTTON: document.querySelector('header img'),
+        NEW_BOOK_MODAL: document.querySelector('dialog'),
+        NEW_BOOK_TITLE: document.getElementById('new-book-title'),
+        NEW_BOOK_AUTHOR: document.getElementById('new-book-author'),
+        NEW_BOOK_READ: document.getElementById('new-book-read'),
+        CANCEL_BTN: document.getElementById('cancel-btn'),
+        SUBMIT_BTN: document.getElementById('submit-btn'),
+    };
 
-    addBook(newBook) {
-        newBook._id = myLibrary._nextBookId;
-        myLibrary._nextBookId += 1;
-        myLibrary.books.push(newBook);
-        ui.refreshLibraryCards();
-    },
+    _UI.NEW_BOOK_BUTTON.addEventListener('click', () => {
+        _UI.NEW_BOOK_MODAL.showModal();
+    });
 
-    removeBook(bookToRemove) {
-        const deleteIndex = myLibrary.books.findIndex((book) => {
-            return bookToRemove._id === book._id;
-        })
-        myLibrary.books.splice(deleteIndex, 1)
-        ui.refreshLibraryCards();
-    },
+    _UI.CANCEL_BTN.addEventListener('click', () => {
+        _UI.NEW_BOOK_MODAL.close();
+        _UI.NEW_BOOK_TITLE.value = '';
+        _UI.NEW_BOOK_AUTHOR.value = '';
+        _UI.NEW_BOOK_READ.checked = false;
+    });
 
-    toggleHaveRead(bookToToggle) {
-        bookToToggle.haveRead = !bookToToggle.haveRead;
-    },
-}
+    _UI.SUBMIT_BTN.addEventListener('click', event => {
+        event.preventDefault();
+        _UI.NEW_BOOK_MODAL.close();
+        const newTitle = _UI.NEW_BOOK_TITLE.value;
+        const newAuthor = _UI.NEW_BOOK_AUTHOR.value;
+        const newRead = _UI.NEW_BOOK_READ.checked;
+        myLibrary.addBook(new Book(newTitle, newAuthor, newRead));
+        _UI.NEW_BOOK_TITLE.value = '';
+        _UI.NEW_BOOK_AUTHOR.value = '';
+        _UI.NEW_BOOK_READ.checked = false;
+    });
 
-ui.initialize();
-// myLibrary.addBook(new Book('Dune', 'Frank Herbert', true));
-// myLibrary.addBook(new Book('Grapes of Wrath', 'John Steinbeck', false));
-// myLibrary.addBook(new Book('Where the Sidewalk Ends', 'Shel Silverstein', false));
-// myLibrary.addBook(new Book('Orlando', 'Virginia Woolf', false));
-// myLibrary.addBook(new Book('Throne of Glass', 'Sarah J. Maas', true));
+    const _addDeleteEventListener = (card, book) => {
+        card.querySelector('button').addEventListener('click', () => {
+                myLibrary.removeBook(book);
+        });
+    };
+
+    const _addReadCheckEventListener = (card, book) => {
+        card.querySelector('input').addEventListener('change', () => {
+                myLibrary.toggleBookHaveRead(book);
+        });
+    };
+
+    const refreshLibraryCards = () => {
+        _UI.MAIN.innerHTML = '';
+        myLibrary.getBooks().forEach(book =>{
+            const newCard = new Card(book);           
+            _addDeleteEventListener(newCard.html, book);
+            _addReadCheckEventListener(newCard.html, book);
+            _UI.MAIN.appendChild(newCard.html);
+        });
+    };
+
+    return {
+        refreshLibraryCards,
+    };
+})();
+
+const myLibrary = (function() {
+    const _books = [];
+    let _nextBookId = 0;
+
+    const getBooks = () => _books;
+
+    const addBook = bookToAdd => {
+        bookToAdd.id = _nextBookId;
+        _nextBookId += 1;
+        _books.push(bookToAdd);
+        displayController.refreshLibraryCards();
+    };
+
+    const removeBook = bookToRemove => {
+        const deleteIndex = _books.findIndex(book => book === bookToRemove);
+        _books.splice(deleteIndex, 1);
+        displayController.refreshLibraryCards();
+    };
+
+    const toggleBookHaveRead = bookToToggle => {bookToToggle.haveRead = !bookToToggle.haveRead};
+
+    return {
+        getBooks,
+        addBook,
+        removeBook,
+        toggleBookHaveRead,
+    };
+})();
