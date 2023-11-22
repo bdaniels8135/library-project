f356d98f0ba0c3fa8d66149883b82be416ba1d90
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-21 20:20:26 -0500

    Move Card display logic to a new file

---
+++ CardsDisplay.js
@@ -0,0 +1,21 @@
+export default function CardsDisplay() {
+  const HTML = document.createElement("div");
+  const cards = [];
+
+  function addCard(cardToAdd) {
+    cards.push(cardToAdd);
+    HTML.appendChild(cardToAdd.HTML);
+  }
+
+  function removeCard(cardToRemove) {
+    const deleteIndex = cards.indexOf(cardToRemove);
+    cards.splice(deleteIndex, 1);
+    HTML.removeChild(HTML.childNodes[deleteIndex]);
+  }
+
+  return {
+    HTML,
+    addCard,
+    removeCard,
+  };
+}
