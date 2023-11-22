254cdf34264218c85adb1f5eabd3cdccbada60d0
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-21 19:25:57 -0500

    Refactor card logic and remove id

--- Card.js
+++ Card.js
@@ -1,36 +1,43 @@
+function buildCardHtml(title, author, haveRead) {
+  const cardHtml = document.createElement("div");
+  cardHtml.classList.add("card");
+
+  const titleElem = document.createElement("p");
+  titleElem.classList.add("title");
+  titleElem.innerText = `${title}`;
+
+  const authorElem = document.createElement("p");
+  authorElem.classList.add("author");
+  authorElem.innerText = `${author}`;
+
+  const checkWrapperElem = document.createElement("div");
+  checkWrapperElem.classList.add("checkbox-wrapper");
+
+  const checkLabelElem = document.createElement("p");
+  checkLabelElem.innerText = "Already Read:";
+
+  const inputElem = document.createElement("input");
+  inputElem.type = "checkbox";
+  inputElem.name = "read-book-check";
+  if (haveRead) inputElem.checked = true;
+
+  const btnElem = document.createElement("button");
+  btnElem.type = "button";
+
+  const imgElem = document.createElement("img");
+  imgElem.src = "./img/trash-can-outline.svg";
+  imgElem.alt = "Trash Icon";
+
+  cardHtml.append(titleElem, authorElem, checkWrapperElem, btnElem);
+  checkWrapperElem.append(checkLabelElem, inputElem);
+  btnElem.appendChild(imgElem);
+
+  return cardHtml;
+}
+
 export default class Card {
   constructor(book) {
-    this.html = document.createElement("div");
-    this.html.classList.add("card");
-    this.html.setAttribute("id", `card-${book.id}`);
-    const titleElem = document.createElement("p");
-    titleElem.classList.add("title");
-    titleElem.innerText = `${book.title}`;
-    const authorElem = document.createElement("p");
-    authorElem.classList.add("author");
-    authorElem.innerText = `${book.author}`;
-    const checkWrapperElem = document.createElement("div");
-    checkWrapperElem.classList.add("checkbox-wrapper");
-    const checkLabelElem = document.createElement("p");
-    checkLabelElem.innerText = "Already Read:";
-    const inputElem = document.createElement("input");
-    inputElem.setAttribute("type", "checkbox");
-    inputElem.setAttribute("name", "read-book-check");
-    inputElem.setAttribute("id", `read-book-check-${book.id}`);
-    if (book.haveRead) inputElem.setAttribute("checked", "");
-    const btnElem = document.createElement("button");
-    btnElem.setAttribute("type", "button");
-    btnElem.setAttribute("id", `trash-button-${book.id}`);
-    const imgElem = document.createElement("img");
-    imgElem.setAttribute("src", "./img/trash-can-outline.svg");
-    imgElem.setAttribute("alt", "Trash Icon");
-
-    this.html.appendChild(titleElem);
-    this.html.appendChild(authorElem);
-    this.html.appendChild(checkWrapperElem);
-    this.html.appendChild(btnElem);
-    checkWrapperElem.appendChild(checkLabelElem);
-    checkWrapperElem.appendChild(inputElem);
-    btnElem.appendChild(imgElem);
+    const { title, author, haveRead } = book;
+    this.html = buildCardHtml(title, author, haveRead);
   }
 }
