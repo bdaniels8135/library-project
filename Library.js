fd88df228f84a869d970a223b5df0ff404ac52e9
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-11-21 19:26:08 -0500

    Book id was never used

--- Library.js
+++ Library.js
@@ -2,14 +2,11 @@ import Book from "./Book.js";
 
 export default function Library() {
   const books = [];
-  let nextBookId = 0;
 
   const getBooks = () => books;
 
   const addBook = (title, author, haveRead) => {
     const bookToAdd = new Book(title, author, haveRead);
-    bookToAdd.id = nextBookId;
-    nextBookId += 1;
     books.push(bookToAdd);
   };
 
